b622de1fe401bcc714c860de879e37400573f90b
slowrunr <mungaslov.nft@gmail.com>
2023-07-17 00:02:24 +0900

    refactored app after watching explainer video

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let addButton = document.querySelector(".add__value");
-let counterText = document.querySelector(".counter");
-let resetButton = document.querySelector(".reset");
-//console.log(button);
-addButton.addEventListener("click", function () {
-  counter = counter + 1;
+const counterText = document.getElementById("counterValue");
+const addValueButton = document.getElementById("addValueButton"); // если обращаемся к id то .getElementById
+const resetButton = document.getElementById("resetButton");
+
+const COUNTER_INITIAL_VALUE = 0; // все конкретные цифры выносятся в отдельные переменные
+let counter = COUNTER_INITIAL_VALUE; // вместо записи 'counter = 0'
+const COUNTER_ADDED_VALUE = 1;
+
+addValueButton.addEventListener("click", function () {
+  counter = counter + COUNTER_ADDED_VALUE; //вариант с parseint(addValueButton.dataset.pool) выдаёт NaN
   //console.log(counter);
   counterText.innerText = counter;
 });
-let counter = 0;
+
 resetButton.addEventListener("click", function () {
-  counter = 0;
+  counter = COUNTER_INITIAL_VALUE;
   counterText.innerText = counter;
 });
