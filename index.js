5bf17b10d385147ac01a8840567c848e37965010
slowrunr <mungaslov.nft@gmail.com>
2023-07-16 22:22:04 +0900

    counterApp done.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-let button = document.querySelector("button");
+let addButton = document.querySelector(".add__value");
 let counterText = document.querySelector(".counter");
+let resetButton = document.querySelector(".reset");
 //console.log(button);
-button.addEventListener("click", function () {
+addButton.addEventListener("click", function () {
   counter = counter + 1;
-  console.log(counter);
+  //console.log(counter);
   counterText.innerText = counter;
 });
-let counter = 0; //объявляем первую переменную
-//DOM document object model
+let counter = 0;
+resetButton.addEventListener("click", function () {
+  counter = 0;
+  counterText.innerText = counter;
+});
